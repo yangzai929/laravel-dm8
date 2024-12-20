f3a3498a2e096fcbd856d16bf906149ec8d824ff
yangzai <755792463@qq.com>
2024-12-20 11:32:13 +0800

    init

---
+++ README.md
@@ -0,0 +1,88 @@
+# 通过Laravel-DM8为Laravel 提供 达梦数据库驱动程序包
+
+**前置条件：**
+
+> 请确保当前环境的PHP 已正确加载DM PHP 模块，即执行php -m 需要显示一下拓展已开启：
+>
+> ```shell
+> PDO                                                                                                                     
+> PDO_DM  
+> ```
+>
+> 如果没有请先安装达梦php驱动和拓展。[安装文档](https://eco.dameng.com/document/dm/zh-cn/pm/php-rogramming-guide.html#6.2%20DM%20PHP%20%E6%A8%A1%E5%9D%97%E5%8A%A0%E8%BD%BD)
+
+## 查询写法
+
+> 1. 使用 `selectRaw` 或者 `DB::raw` 函数查询时，如果当前数据库是忽略大小写的，一律返回小写字段名，
+
+## 最新版
+
+> 1. 修复model::create() 方法新增数据报错
+
+## 关于Laravel-DM8
+
+Laravel-DM8 是 [Laravel](http://laravel.com/) 的达梦数据库驱动程序包。它是 
+ [Illuminate/Database](https://github.com/illuminate/database) 的拓展，它使用 [DM8](https://eco.dameng.com/document/dm/zh-cn/faq/faq-php.html#PHP-Startup-Unable-to-load-dynamic-library) 拓展与 达梦数据库通信. 基于[Laravel的Oracle的数据库驱动程序包](https://github.com/yajra/laravel-oci8)修改而来 ,感谢 [@yajra](https://github.com/yajra).
+
+## 文档
+
+- [DM8 程序员手册](https://eco.dameng.com/document/dm/zh-cn/pm/programmer-overview.html)
+
+## Laravel 版本兼容性
+
+ Laravel | Package 
+:--------|:--------
+ 5.1.x   | 5.1.x   
+ 5.2.x   | 5.2.x   
+ 5.3.x   | 5.3.x   
+ 5.4.x   | 5.4.x   
+ 5.5.x   | 5.5.x   
+ 5.6.x   | 5.6.x   
+ 5.7.x   | 5.7.x   
+ 5.8.x   | 5.8.x   
+ 6.x.x   | 6.x.x   
+ 7.x.x   | 7.x.x   
+ 8.x.x   | 8.x.x   
+ 9.x.x   | 9.x.x   
+
+## 快速安装
+
+```bash
+composer require dennyyang/laravel-dm8
+```
+
+## 服务提供商（Laravel 5.5+ 上可选）
+
+通过 Composer 安装或更新了您的软件包，您就需要注册 Laravel-DM8。打开`config/app.php`并找到 providers 并添加：
+
+```php
+LaravelDm8\\Dm8\\Dm8ServiceProvider::class,
+```
+
+## 配置（可选）
+
+您可以选择通过运行以下 Artisan 命令来发布配置文件。如果配置文件未发布，包将自动使用`.env`文件 数据库配置中声明的内容。
+
+```bash
+php artisan vendor:publish --tag=dm
+```
+
+这会将配置文件复制到`config/dm.php`。
+
+> 然后，您可以在`.env`文件中设置连接数据：
+
+```ini
+DB_CONNECTION = dm
+DB_HOST = dm.host
+DB_PORT = 5236
+DB_DATABASE = xe
+DB_USERNAME = hr
+DB_PASSWORD = hr
+DB_CHARSET = UTF8
+```
+
+之后可以运行你的Laravel 程序了。
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE.md) for more information.
---
+++ src/Dm8/Query/Processors/DmProcessor.php
@@ -0,0 +1,205 @@
+<?php
+
+namespace LaravelDm8\Dm8\Query\Processors;
+
+use DateTime;
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Query\Builder;
+use Illuminate\Database\Query\Processors\Processor;
+use PDO;
+
+class DmProcessor extends Processor
+{
+    /**
+     * Process an "insert get ID" query.
+     *
+     * @param  Builder  $query
+     * @param  string  $sql
+     * @param  array  $values
+     * @param  string  $sequence
+     * @return int
+     */
+     public function processInsertGetId(Builder $query, $sql, $values, $sequence = null)
+     {
+         $connection = $query->getConnection();
+
+         $connection->recordsHaveBeenModified();
+         $start = microtime(true);
+
+         $id = 0;
+         $parameter = 1;
+         $statement = $this->prepareStatement($query, $sql);
+         $values = $this->incrementBySequence($values, $sequence);
+         $parameter = $this->bindValues($values, $statement, $parameter);
+         $statement->bindParam($parameter, $id, PDO::PARAM_INT, -1);
+         $statement->execute();
+
+         $values[] = '?';
+         $connection->logQuery($sql, $values, $start);
+
+         return (int) $id;
+     }
+
+    /**
+     * Get prepared statement.
+     *
+     * @param  Builder  $query
+     * @param  string  $sql
+     * @return \PDOStatement|\Yajra\Pdo\Oci8
+     */
+    private function prepareStatement(Builder $query, $sql)
+    {
+        /** @var \LaravelDm8\Dm8\Dm8Connection $connection */
+        $connection = $query->getConnection();
+        $pdo = $connection->getPdo();
+
+        return $pdo->prepare($sql);
+    }
+
+    /**
+     * Insert a new record and get the value of the primary key.
+     *
+     * @param  array  $values
+     * @param  string  $sequence
+     * @return array
+     */
+    protected function incrementBySequence(array $values, $sequence)
+    {
+        $builder = debug_backtrace(DEBUG_BACKTRACE_PROVIDE_OBJECT, 5)[3]['object'];
+        $builderArgs = debug_backtrace(DEBUG_BACKTRACE_PROVIDE_OBJECT, 5)[2]['args'];
+
+        if (! isset($builderArgs[1][0][$sequence])) {
+            if ($builder instanceof EloquentBuilder) {
+                /** @var \LaravelDm8\Dm8\Eloquent\DmEloquent $model */
+                $model = $builder->getModel();
+                /** @var \LaravelDm8\Dm8\Dm8Connection $connection */
+                $connection = $model->getConnection();
+                if ($model->sequence && $model->incrementing) {
+                    $values[] = (int) $connection->getSequence()->nextValue($model->sequence);
+                }
+            }
+        }
+
+        return $values;
+    }
+
+    /**
+     * Bind values to PDO statement.
+     *
+     * @param  array  $values
+     * @param  \PDOStatement  $statement
+     * @param  int  $parameter
+     * @return int
+     */
+    private function bindValues(&$values, $statement, $parameter)
+    {
+        $count = count($values);
+        for ($i = 0; $i < $count; $i++) {
+            if (is_object($values[$i])) {
+                if ($values[$i] instanceof DateTime) {
+                    $values[$i] = $values[$i]->format('Y-m-d H:i:s');
+                } else {
+                    $values[$i] = (string) $values[$i];
+                }
+            }
+            $type = $this->getPdoType($values[$i]);
+            $statement->bindParam($parameter, $values[$i], $type);
+            $parameter++;
+        }
+
+        return $parameter;
+    }
+
+    /**
+     * Get PDO Type depending on value.
+     *
+     * @param  mixed  $value
+     * @return int
+     */
+    private function getPdoType($value)
+    {
+        if (is_int($value)) {
+            return PDO::PARAM_INT;
+        }
+
+        if (is_bool($value)) {
+            return PDO::PARAM_BOOL;
+        }
+
+        if (is_null($value)) {
+            return PDO::PARAM_NULL;
+        }
+
+        return PDO::PARAM_STR;
+    }
+
+    /**
+     * Save Query with Blob returning primary key value.
+     *
+     * @param  Builder  $query
+     * @param  string  $sql
+     * @param  array  $values
+     * @param  array  $binaries
+     * @return int
+     */
+    public function saveLob(Builder $query, $sql, array $values, array $binaries)
+    {
+        $connection = $query->getConnection();
+
+        $connection->recordsHaveBeenModified();
+        $start = microtime(true);
+
+        $id = 0;
+        $parameter = 1;
+        $statement = $this->prepareStatement($query, $sql);
+
+        $parameter = $this->bindValues($values, $statement, $parameter);
+
+        $countBinary = count($binaries);
+        for ($i = 0; $i < $countBinary; $i++) {
+            $statement->bindParam($parameter, $binaries[$i], PDO::PARAM_LOB, -1);
+            $parameter++;
+        }
+
+        // bind output param for the returning clause.
+        $statement->bindParam($parameter, $id, PDO::PARAM_INT, -1);
+
+        if (! $statement->execute()) {
+            return false;
+        }
+
+        $connection->logQuery($sql, $values, $start);
+
+        return (int) $id;
+    }
+
+    /**
+     * Process the results of a column listing query.
+     *
+     * @param  array  $results
+     * @return array
+     */
+    public function processColumnListing($results)
+    {
+        $mapping = function ($r) {
+            if (is_array($r)) {
+                // 如果 $r 是数组，检查是否有 'COLUMN_NAME' 键
+                $columnName = $r['column_name'] ?? $r['COLUMN_NAME'] ?? null;
+            } elseif (is_object($r)) {
+                // 如果 $r 是对象，检查是否有 column_name 属性
+                $columnName = $r->column_name ?? $r->COLUMN_NAME ?? null;
+            } else {
+                // 返回 null 表示结构不匹配
+                $columnName = null;
+            }
+
+            if (!$columnName) {
+                throw new \Exception("Missing 'column_name' field in column listing.");
+            }
+
+            return strtolower($columnName);
+        };
+
+        return array_map($mapping, $results);
+    }
+}
